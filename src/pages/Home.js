b3063f3048f9235949b37bdf1ad34a025a5dc9a0
Ilook18 <anirudhanand984@gmail.com>
2021-02-18 19:21:46 +0530

    displayed search results

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,43 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
 
   const onInputChange = ev => {
     setInput(ev.target.value);
   };
 
   const onSearch = () => {
-    // http://api.tvmaze.com/search/shows?q=girls
-
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(r => r.json())
-      .then(result => {
-        // eslint-disable-next-line no-console
-        console.log(result);
-      });
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+    });
   };
 
   const onKeyDown = ev => {
     if (ev.keyCode === 13) {
       onSearch();
     }
+  };
+
+  const renderResult = () => {
+    if (results && results.length === 0) {
+      return <div> Not found </div>;
+    }
+
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+
+    return null;
   };
 
   // eslint-disable-next-line arrow-body-style
@@ -38,6 +53,7 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResult()}
     </MainPageLayout>
   );
 };
